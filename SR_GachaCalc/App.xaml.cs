eccd77e3baf78eddd7b3da49dc4651f7a980e43c
York <zhuyanfang@outlook.com>
2023-09-18 19:19:49 +0800

    Initial Commit

---
+++ SR_GachaCalc/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SR_GachaCalc
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ SR_GachaCalc/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace SR_GachaCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
