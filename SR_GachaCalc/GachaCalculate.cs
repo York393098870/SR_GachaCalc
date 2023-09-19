19d093fd140ea03dca433387da7098cb25b92423
York <zhuyanfang@outlook.com>
2023-09-19 17:55:39 +0800

    Finish basic gacha system.

--- SR_GachaCalc/GachaCalculate.cs
+++ SR_GachaCalc/GachaCalculate.cs
@@ -5,6 +5,7 @@ namespace SR_GachaCalc;
 public class GachaCalculate
 {
     private static int total_gacha_times; //定义总抽卡次数
+    private static Random _random = new Random();
 
     //传入单次五星循环内抽卡的次数，获取单抽的概率
     public static decimal get_succeed_probability(int singleGachaTimes)
@@ -35,5 +36,31 @@ public class GachaCalculate
                 Environment.Exit(1); //返回错误代码1，单次循环内不允许的输入
                 return -1m;
         }
+    }
+
+    public static bool get_single_result(decimal probability)
+    {
+        return _random.NextDouble() < (double)probability;
+    }
+
+    public static int gacha(int times)
+    {
+        total_gacha_times = times;
+        int success_times = 0;
+        var n = 1; //n为出金循环内抽卡次数
+        for (var i = 1; i <= times; i++) //i为总抽卡次数
+        {
+            if (get_single_result(get_succeed_probability(n)))
+            {
+                success_times++;
+                n = 1; //重置出金循环n次数
+            }
+            else
+            {
+                n++;
+            }
+        }
+
+        return success_times;
     }
 }
--- SR_GachaCalc/MainWindow.xaml.cs
+++ SR_GachaCalc/MainWindow.xaml.cs
@@ -12,7 +12,10 @@ namespace SR_GachaCalc
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(GachaCalculate.get_succeed_probability(0));
+            //Console.WriteLine(GachaCalculate.gacha(100000));
+            const int times = 10000000;
+            var averageTimes = Math.Round((double)times / GachaCalculate.gacha(times), 2);
+            Console.WriteLine("总共模拟抽卡:" + times + "次，获得五星角色的期望抽数为：" + averageTimes + "抽");
         }
     }
 }
