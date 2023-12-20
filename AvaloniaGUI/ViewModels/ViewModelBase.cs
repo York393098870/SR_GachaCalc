8793832475781563b0c4a447cfafaaa8974dde0d
York393098870 <122362831+York393098870@users.noreply.github.com>
2023-12-20 20:38:58 +0800

    重构UI界面

---
+++ AvaloniaGUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaGUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ AvaloniaGUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaGUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
